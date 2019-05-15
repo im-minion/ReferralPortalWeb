4bcfc3aba1019a173393a9102294f63590e93de9
im-minion <vaibhavminiyar@gmail.com>
2019-05-15 09:52:29 +0530

    footer and login component addded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
