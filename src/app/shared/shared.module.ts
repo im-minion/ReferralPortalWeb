b0958952881b93573d8718a7a42fbea39b2dec7a
im-minion <vaibhavminiyar@gmail.com>
2019-06-02 01:19:48 +0530

    got what is container..added success-message component inside shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SuccessMessageComponent } from './success-message/success-message.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SuccessMessageComponent],
   imports: [
     CommonModule
   ]
