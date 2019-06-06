51840ace59cd5b6c70eba678d8d8110608026392
im-minion <vaibhavminiyar@gmail.com>
2019-06-06 23:49:09 +0530

    added hosted url in env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false
 };
-
+// https://referral-portal-ws.herokuapp.com/
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
