239df0c225a83fdd1777de15c00aacec2d04af06
im-minion <vaibhavminiyar@gmail.com>
2019-06-11 23:22:37 +0530

    added the reactive forms in comment

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -11,5 +11,31 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
   }
+  /*
+     ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            confirmPassword: ['', Validators.required]
+        }, {
+            validator: MustMatch('password', 'confirmPassword')
+        });
+    }
 
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+    }
+  */
 }
