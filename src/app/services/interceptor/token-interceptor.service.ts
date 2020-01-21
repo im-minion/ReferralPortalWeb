821b739801663b1276d3cce7092b9d400036b216
im-minion <vaibhavminiyar@gmail.com>
2020-01-21 11:14:20 +0530

    dashboard UI and interceptor modified for login

--- src/app/services/interceptor/token-interceptor.service.ts
+++ src/app/services/interceptor/token-interceptor.service.ts
@@ -5,15 +5,19 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class TokenInterceptorService implements HttpInterceptor{
+export class TokenInterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let tokenizedReq = req.clone({
+    if (req.url.includes('login')) {
+      return next.handle(req);
+    } else {
+      let tokenizedReq = req.clone({
         setHeaders: {
           Authorization: 'Bearer ' + localStorage.getItem('token')
         }
-    });
-    return next.handle(tokenizedReq);
+      });
+      return next.handle(tokenizedReq);
+    }
   }
 
   constructor() { }
