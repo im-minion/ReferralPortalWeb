181f53c139f183b2eac9c970cddf4dbb0ceb8f6b
im-minion <vaibhavminiyar@gmail.com>
2020-02-12 20:59:37 +0530

    donw with shared component

--- src/app/referrals/referrals.component.ts
+++ src/app/referrals/referrals.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { EmployeeService } from '../services/employee-services/employee.service';
 import { Referrals } from '../utilities/referrals-class';
-import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
-import { isNullOrUndefined } from 'util';
 @Component({
   selector: 'app-referrals',
   templateUrl: './referrals.component.html',
@@ -11,30 +9,14 @@ import { isNullOrUndefined } from 'util';
 export class ReferralsComponent implements OnInit {
   isLoading: boolean = true;
   displayedColumns: string[] = ['Referral Name', 'Job Id', 'Current Level', 'Current Status', 'Progress'];
-  dataSource: MatTableDataSource<Referrals> = null;
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+  dataSource: Array<Referrals> = null;
 
   constructor(private employeeService: EmployeeService) { }
 
   ngOnInit() {
     this.employeeService.getReferralsByEmployeeId(sessionStorage.getItem('employeeId')).subscribe((response: Array<Referrals>) => {
-      console.log(response);
-      this.dataSource = new MatTableDataSource(response);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+      this.dataSource = response;
       this.isLoading = false;
     });
-  }
-
-  applyFilter(filterValue: string) {
-    if (!isNullOrUndefined(this.dataSource)) {
-      this.dataSource.filter = filterValue.trim().toLowerCase();
-
-      if (this.dataSource.paginator) {
-        this.dataSource.paginator.firstPage();
-      }
-    }
   }
 }
